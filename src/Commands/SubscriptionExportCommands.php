47e26434556e7e4d2eec12181e1db63ff91a578a
Qrolic <info@qrolic.com>
2021-01-19 14:25:06 +0530

    Updated Documentation

--- src/Commands/SubscriptionExportCommands.php
+++ src/Commands/SubscriptionExportCommands.php
@@ -24,17 +24,26 @@ class SubscriptionExportCommands extends WP_CLI_Command {
      * : To run script for all subscriptions.
      *
      * [--data-source]
-     * : Run for specific data source.
+     * : Run for specific data source, pass the datasource UUID from ChartMogul.
      *
      * [--create-data-source]
      * : To create data source.
      *
      * ## EXAMPLES
      *
-     *     # Export subscriptions to ChartMogul.
-     *     $ wp cxl shop-subscription-export-chartmogul --id=25
+     *     # Export single subscriptions to ChartMogul.
+     *     $ wp cxl shop-subscription-export-chartmogul --id=25 --data-source=xxxxxx
      *     Subscription #25 sent to ChartMogul.
      *
+     *     # Export all subscriptions to ChartMogul.
+     *     $ wp cxl shop-subscription-export-chartmogul --all --data-source=xxxxxx
+     *     Subscription #25 sent to ChartMogul.
+     *
+     *     # Create Datasource in ChartMogul.
+     *     $ wp cxl shop-subscription-export-chartmogul --create-data-source=true --data-source=new-datasource
+     *     Success: Data source created successfully.
+     *
+     * @todo  Separate command for create data source.
      * @param array $args
      * @param array $assoc_args
      * @subcommand shop-subscription-export-chartmogul
