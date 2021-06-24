6a75eb213fee999ff06b9132e90c9c6855468d3e
saasfreelancer <1642796+saas786@users.noreply.github.com>
2021-06-24 20:10:57 +0500

    chore: move bootstrap-plugin.php to Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -7,6 +7,8 @@
 
 namespace CXL\WC\ChartMogul;
 
+use CXL\WC\ChartMogul\Core\Application;
+
 defined( 'ABSPATH' ) || exit;
 
 /**
@@ -33,6 +35,12 @@ final class Plugin {
 	 * Singleton pattern
 	 */
 	private static ?Plugin $instance = null;
+
+	/**
+	 * A reference to the Application instance.
+	 *
+	 */
+	public static ?Application $app = null;
 
 	/**
 	 * Constructor.
@@ -87,15 +95,54 @@ final class Plugin {
 	}
 
 	public function init(): void {
-		// ------------------------------------------------------------------------------
-		// Bootstrap the plugin.
-		// ------------------------------------------------------------------------------
-		//
-		// Load the bootstrap files. Note that autoloading should happen first so that
-		// any classes/functions are available that we might need.
 
-		/** @psalm-suppress UnresolvableInclude */
-		require_once $this->plugin_dir_path . 'src/bootstrap-plugin.php';
+		/**
+		 * ------------------------------------------------------------------------------
+		 * Create a new application.
+		 * ------------------------------------------------------------------------------
+		 *
+		 * Creates the one true instance of the Hybrid Core application. You may access
+		 * this instance via the `\CXL\WC\ChartMogul\Plugin\App` static class
+		 * after the application has booted.
+		 *
+		 */
+		$app = new Application();
+
+		/**
+		 * ------------------------------------------------------------------------------
+		 * Register service providers with the application.
+		 * ------------------------------------------------------------------------------
+		 *
+		 * Before booting the application, add any service providers that are necessary
+		 * for running the plugin. Service providers are essentially the backbone of the
+		 * bootstrapping process.
+		 */
+		$app->provider(Provider::class);
+
+		/**
+		 * ------------------------------------------------------------------------------
+		 * Perform bootstrap actions.
+		 * ------------------------------------------------------------------------------
+		 *
+		 * Creates an action hook for parent/child themes (or even plugins) to hook into the
+		 * bootstrapping process and add their own bindings before the app is booted by
+		 * passing the application instance to the action callback.
+		 *
+		 */
+		do_action('cxl/wc/chartmogul/bootstrap', $app);
+
+		/**
+		 * ------------------------------------------------------------------------------
+		 * Bootstrap the application.
+		 * ------------------------------------------------------------------------------
+		 *
+		 * Calls the application `boot()` method, which launches the application. Pat
+		 * yourself on the back for a job well done.
+		 *
+		 */
+		$app->boot();
+
+		self::$app = $app;
 	}
 
 	/**
